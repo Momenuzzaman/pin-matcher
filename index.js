c9ac30073491766a43c647bd183a052e20ffe7f9
mdemon420 <mdeamon4@gmail.com>
2021-05-11 01:39:34 +0600

    done

--- index.js
+++ index.js
@@ -22,12 +22,39 @@ buttonContainer.addEventListener("click",function(event){
     if(isNaN(digit)){
         // handle backspace
         // handle clear
+        if(digit === "C"){
+            const typedInput = document.getElementById("typed-pin");
+            typedInput.value = "";
+        }
+       
+
     }
     else{
         const typedInput = document.getElementById("typed-pin");
         typedInput.value = typedInput.value + digit;
     }
 });
+//  veify pin
+function varifyPin(){
+    const pin =  document.getElementById("pin").value;
+   const typedPin = document.getElementById("typed-pin").value;
+   if(pin === typedPin){
+        const correct = document.getElementById("correct");
+        correct.style.display = "block";
+        const incorrect = document.getElementById("incorrect");
+        incorrect.style.display = "none";
+       
+        
+   }
+   else{
+         const correct = document.getElementById("correct");
+        correct.style.display = "none";
+        const incorrect = document.getElementById("incorrect");
+        incorrect.style.display = "block";
+       
+        
+   }
+}
 
 
 

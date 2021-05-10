44e327e5d24b5485e890fcfe3204bca339f98710
mdemon420 <mdeamon4@gmail.com>
2021-05-11 00:21:28 +0600

    second Commit

--- index.js
+++ index.js
@@ -12,7 +12,23 @@ function getPin(){
 // display generate pin
 function generatePin(){
     const pinInput = document.getElementById("pin");
-pinInput.value =  getPin();
+    pinInput.value =  getPin();
 }
+
+// handle calculator button event 
+const buttonContainer = document.getElementById("digit-container");
+buttonContainer.addEventListener("click",function(event){
+    const digit =(event.target.innerText);
+    if(isNaN(digit)){
+        // handle backspace
+        // handle clear
+    }
+    else{
+        const typedInput = document.getElementById("typed-pin");
+        typedInput.value = typedInput.value + digit;
+    }
+});
+
+
 
 
